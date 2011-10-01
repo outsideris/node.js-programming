c53e039edbb1580e67445ef434d843f05e57122e
Outsider <outsideris@gmail.com>
2011-10-01 12:23:11 +0900

    socket.io 연결 예제 추가

---
+++ socket.io/connect/app.js
@@ -0,0 +1,58 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    io = require('socket.io');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+app.listen(3000);
+
+io = io.listen(app);
+
+io.configure(function(){
+  io.enable('browser client etag');
+  io.set('log level', 3);
+  io.set('transports', [
+    'websocket'
+    , 'flashsocket'
+    , 'htmlfile'
+    , 'xhr-polling'
+    , 'jsonp-polling'
+    ]);
+});
+
+io.sockets.on('connection', function(socket) {
+  console.log('connected');
+});
+
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
