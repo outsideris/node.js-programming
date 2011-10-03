1e2642c7fa4cb7e11edd02be5ee9221f48e02483
Outsider <outsideris@gmail.com>
2011-10-03 19:09:10 +0900

    socket.io 예제 express제거해서 재작성

---
+++ socket.io/connect/server.js
@@ -0,0 +1,55 @@
+var http = require('http')
+  , fs = require('fs')
+  , path = require('path')
+  , io = require('socket.io');
+
+var server = http.createServer(function(req, res) {
+  var filename = path.join(process.cwd(), req.url);
+
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      res.writeHead(404, {"Content-Type": "text/plain"});
+      res.write("404 Not Found\n");
+      res.end();
+      return;
+    }
+
+    fs.readFile('./index.html', encoding='utf-8', function(err, data) {
+      if (err) {
+        res.writeHead(500, {"Content-Type": "text/plain"});  
+        res.write(err + "\n");  
+        res.end();  
+        return;  
+      } 
+      res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"}); 
+      res.end(data);
+    }); 
+  }); 
+});
+
+server.listen(3000);
+
+io = io.listen(server);
+
+io.configure(function(){
+  io.enable('browser client etag');
+  io.set('log level', 3);
+  io.set('transports', [
+    'websocket'
+    , 'flashsocket'
+    , 'htmlfile'
+    , 'xhr-polling'
+    , 'jsonp-polling'
+    ]);
+
+});
+
+io.sockets.on('connection', function(socket) {
+  console.log('connected');
+  socket.on('disconnect', function() {
+    console.log('Good-bye'); 
+  });
+});
+
+
+console.log("서버가 시작되었습니다. http://localhost:3000");
