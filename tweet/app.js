a22c599f0a3896204618f4da844399e91e0ab8f5
Outsider <outsideris@gmail.com>
2011-10-13 03:33:19 +0900

    twitter 예제에 스케쥴링 추가

--- tweet/app.js
+++ tweet/app.js
@@ -1,5 +1,6 @@
 var rest = require('restler')
   , fs = require('fs')
+  , schedule = require('node-schedule');
 
 var Tweet = {
   sinceId: '1' 
@@ -25,29 +26,42 @@ var Tweet = {
   }
 }
 
-var isOpened = false;
+var FileManager = function() {
+  var isOpened = false;
 
-fs.readFile('./maxid.txt', function(err, data) {
-  if (err) { Tweet.setSinceId(1); }
+  fs.readFile('./maxid.txt', function(err, data) {
+    if (err) { Tweet.setSinceId(1); }
 
-  Tweet.setSinceId(data);
+    Tweet.setSinceId(data);
 
-  Tweet.getTweets('#love', function(data) {
-    var text = "";
-    data.results.forEach(function(elem, index, array) {
-      text += elem.from_user + ': ' + elem.text + ' at' + elem.created_at + '\n';
-    });
-
-    fs.open('./tweets.txt', 'a', 0666, function(err, fd) {
-      if(err) { throw err; }
-      isOpened = true;
-      fs.write(fd, new Buffer(text), null, null, null, function(err) {
-        fs.close(fd, function() {
-          isOpened = false; 
-          fs.writeFile('./maxid.txt', data.max_id.toString(), function(err) {});
-        }); 
+    Tweet.getTweets('#love', function(data) {
+      var text = "";
+      data.results.forEach(function(elem, index, array) {
+        text += elem.from_user + ': ' + elem.text + ' at' + elem.created_at + '\n';
       });
+
+      if (!isOpened) {
+        fs.open('./tweets.txt', 'a', 0666, function(err, fd) {
+          if(err) { throw err; }
+          isOpened = true;
+          fs.write(fd, new Buffer(text), null, null, null, function(err) {
+            fs.close(fd, function() {
+              isOpened = false; 
+              fs.writeFile('./maxid.txt', data.max_id.toString(), function(err) {});
+            }); 
+          });
+        });
+      }
     });
   });
+};
 
+
+var rule = new schedule.RecurrenceRule();
+rule.hour = 2;
+//rule.minute = 42;
+
+var j = schedule.scheduleJob(rule, function(){
+      console.log('The world is going to end today.' + new Date());
 });
+
